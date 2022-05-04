0481f524147392282c118a7980e2f25014c30145
ANLIN-3344 <98148890+ANLIN-3344@users.noreply.github.com>
2022-05-04 13:53:43 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+# Author: Anlin
+# Email: Anlin_harker@qq.com
+# python_version: 3.6
+# module: pygamepro
+# need_main_module: pygame, os, pywin32, ETC
+# Thank you for your use!
+
+This is a module what can help you good to use pygame
+"""
+
+from typing import Union
+from pygamepro.parameter import *
+from pygamepro.window import *
+from pygamepro.effects import *
+
+import pygame
